7cef6a459cf09144fe28381ed790a5943a4b6dca
MuhammadAlfiFauzul <alfikerja12@gmail.com>
2025-09-16 02:38:10 +0700

    Update Loaderyahayuk.lua

--- Loaderyahayuk.lua
+++ Loaderyahayuk.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/AlfiFazulul/script/refs/heads/main/jembut.lua"
+local url = "https://raw.githubusercontent.com/AlfiFazulul/script/refs/heads/main/jembutyahayuk.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
